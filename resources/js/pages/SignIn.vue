050d70b4755d84890527a2a46258fad41b93b10c
Cong Do <dochauthanhcong147@gmail.com>
2020-06-18 02:07:09 +0700

    - Change Login form to Ant Design

--- resources/js/pages/SignIn.vue
+++ resources/js/pages/SignIn.vue
@@ -6,48 +6,89 @@
         <!-- /.login-logo -->
         <div class="login-box-body">
             <p class="login-box-msg">{{ $t('login.login_box') }}</p>
-
             <ValidationObserver v-slot="{ handleSubmit }">
-                <form @submit.prevent="handleSubmit(submit)">
-                    <ValidationProvider name="ログインID" rules="required|min:3|max:100" v-slot="{ errors }">
-                        <div class="form-group has-feedback" :class="{'has-error': errors[0]}">
-                            <input
-                                type="text"
-                                class="form-control"
-                                id="email"
-                                :placeholder="$t('login.email_placeholder')"
-                                name="ログインID"
-                                v-model="inputData.email">
-                            <span class="fa fa-user form-control-feedback"></span>
+                <a-form
+                    id="components-form-demo-normal-login"
+                    class="login-form"
+                    @submit.prevent="handleSubmit(submit)"
+                >
+                    <a-form-item has-feedback validate-status="error" help="Ko dau">
+                        <ValidationProvider name="ログインID" rules="required|email" v-slot="{ errors }">
+                            <a-input :placeholder="$t('login.email_placeholder')" v-model="inputData.email">
+                                <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)" />
+                            </a-input>
                             <span v-show="errors[0]" :class="{'help-block': errors[0] }">
-                                {{ errors[0] }}
+                                                                {{ errors[0] }}
                             </span>
-                        </div>
-                    </ValidationProvider>
-
-                    <ValidationProvider name="ログインパスワード" rules="required|min:3|max:100" v-slot="{ errors }">
-                        <div class="form-group has-feedback" :class="{'has-error': errors[0]}">
-                            <input
-                                type="password"
-                                class="form-control"
-                                id="password"
-                                :placeholder="$t('login.password_placeholder')"
-                                name="ログインパスワード"
-                                v-model="inputData.password">
-                            <span class="fa fa-user form-control-feedback"></span>
+                        </ValidationProvider>
+                    </a-form-item>
+                    <a-form-item>
+                        <ValidationProvider name="ログインパスワード" rules="required|min:3|max:100" v-slot="{ errors }">
+                            <a-input type="password" :placeholder="$t('login.password_placeholder')" v-model="inputData.password">
+                                <a-icon slot="prefix" type="lock" style="color: rgba(0,0,0,.25)" />
+                            </a-input>
                             <span v-show="errors[0]" :class="{'help-block': errors[0] }">
-                                {{ errors[0] }}
+                                                                {{ errors[0] }}
                             </span>
-                        </div>
-                    </ValidationProvider>
+                        </ValidationProvider>
+                    </a-form-item>
+                    <a-form-item>
+                        <a-checkbox>
+                            Remember me
+                        </a-checkbox>
+                        <a class="login-form-forgot" href="">
+                            Forgot password
+                        </a>
+                        <a-button type="primary" html-type="submit" class="login-form-button">
+                            {{ $t('login.login_button') }}
+                        </a-button>
+                        Or
+                        <a href="">
+                            register now!
+                        </a>
+                    </a-form-item>
+                </a-form>
 
-                    <button type="submit" class="btn btn-primary btn-block btn-flat">{{ $t('login.login_button') }}</button>
-                    <div class="row">
-                        <div class="col-xs-12">
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </form>
+<!--                <form @submit.prevent="handleSubmit(submit)">-->
+<!--                    <ValidationProvider name="ログインID" rules="required|min:3|max:100" v-slot="{ errors }">-->
+<!--                        <div class="form-group has-feedback" :class="{'has-error': errors[0]}">-->
+<!--                            <input-->
+<!--                                type="text"-->
+<!--                                class="form-control"-->
+<!--                                id="email"-->
+<!--                                :placeholder="$t('login.email_placeholder')"-->
+<!--                                name="ログインID"-->
+<!--                                v-model="inputData.email">-->
+<!--                            <span class="fa fa-user form-control-feedback"></span>-->
+<!--                            <span v-show="errors[0]" :class="{'help-block': errors[0] }">-->
+<!--                                {{ errors[0] }}-->
+<!--                            </span>-->
+<!--                        </div>-->
+<!--                    </ValidationProvider>-->
+
+<!--                    <ValidationProvider name="ログインパスワード" rules="required|min:3|max:100" v-slot="{ errors }">-->
+<!--                        <div class="form-group has-feedback" :class="{'has-error': errors[0]}">-->
+<!--                            <input-->
+<!--                                type="password"-->
+<!--                                class="form-control"-->
+<!--                                id="password"-->
+<!--                                :placeholder="$t('login.password_placeholder')"-->
+<!--                                name="ログインパスワード"-->
+<!--                                v-model="inputData.password">-->
+<!--                            <span class="fa fa-user form-control-feedback"></span>-->
+<!--                            <span v-show="errors[0]" :class="{'help-block': errors[0] }">-->
+<!--                                {{ errors[0] }}-->
+<!--                            </span>-->
+<!--                        </div>-->
+<!--                    </ValidationProvider>-->
+
+<!--                    <button type="submit" class="btn btn-primary btn-block btn-flat">{{ $t('login.login_button') }}</button>-->
+<!--                    <div class="row">-->
+<!--                        <div class="col-xs-12">-->
+<!--                        </div>-->
+<!--                        &lt;!&ndash; /.col &ndash;&gt;-->
+<!--                    </div>-->
+<!--                </form>-->
             </ValidationObserver>
         </div>
         <!-- /.login-box-body -->
@@ -62,7 +103,9 @@
 
     export default {
         name: 'SignIn',
-
+        beforeCreate() {
+            this.form = this.$form.createForm(this, { name: 'normal_login' });
+        },
         data() {
             return {
                 inputData: {
@@ -93,3 +136,14 @@
         },
     }
 </script>
+<style>
+    #components-form-demo-normal-login .login-form {
+        max-width: 300px;
+    }
+    #components-form-demo-normal-login .login-form-forgot {
+        float: right;
+    }
+    #components-form-demo-normal-login .login-form-button {
+        width: 100%;
+    }
+</style>
